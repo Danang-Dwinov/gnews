b322173ba9171aa6b0cf04c56001f136a87f9621
Danang Dwinov <danangdarren@gmail.com>
2026-08-16 20:04:04 +0700

    Fixed the buttons on the footer to work

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <div class="footer-list" id="footer-list">
-    <i class="fa-solid fa-house" id="footer-home"></i>
-    <i class="fa-solid fa-user"id="footer-profile"></i>
+    <a href="/"><i class="fa-solid fa-house" id="footer-home"></i></a>
+    <a href="/profile"><i class="fa-solid fa-user" id="footer-profile"></i></a>
   </div>
 </footer>
