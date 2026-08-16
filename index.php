485b417ccce9d7d74fdb9784cb76e61c5715ea34
Danang Dwinov <danangdarren@gmail.com>
2026-08-16 13:03:00 +0700

    Delete session, create path link for user when request url

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-session_start();
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if(isset($_SESSION['username'])){
-  header("Location: pages/home.php");
-  exit;
+switch ($request) {
+    case '/':
+        require __DIR__ . '/pages/home.php';
+        break;
+    case '/profile':
+        require __DIR__ . '/pages/profile.php';
+        break;
+    case '/auth':
+        require __DIR__ . '/pages/auth.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/pages/404.php';
 }
-header("Location: pages/auth.php");
-exit;
 ?>
