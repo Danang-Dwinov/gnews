73e1e18403b7f5dfa9c9fd9574ae5b2f814c2344
Danang Dwinov <danangdarren@gmail.com>
2026-08-16 13:26:51 +0700

    Add footer

---
+++ components/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="footer-list" id="footer-list">
+    <i class="fa-solid fa-house" id="footer-home"></i>
+    <i class="fa-solid fa-user"id="footer-profile"></i>
+  </div>
+</footer>
