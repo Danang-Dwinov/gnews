7b429800a559a3486f2e3fb67bdac9267a8d1b96
Danang Dwinov <danangdarren@gmail.com>
2026-08-16 20:01:23 +0700

    Add short circuit routing for /logout

--- router.php
+++ router.php
@@ -1,5 +1,11 @@
 <?php
 $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($request === '/logout') {
+  require __DIR__ . '/pages/logout.php';
+  exit;
+}
+
 $file = __DIR__ . $request;
 
 if ($request !== '/' && is_file($file)){
