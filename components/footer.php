31d5216d963acf2b119ba8bf047adf817f123959
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-17 19:58:43 +0700

    Add icon search

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
   <div class="footer-list" id="footer-list">
     <a href="/"><i class="fa-solid fa-house" id="footer-home"></i></a>
+    <a href="/search"><i class="fa-solid fa-magnifying-glass" id="footer-search"></i></a>
     <a href="/profile"><i class="fa-solid fa-user" id="footer-profile"></i></a>
   </div>
 </footer>
