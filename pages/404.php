7fdd7482de7bffe1b896150aa02bd4861d1d249a
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-17 19:57:57 +0700

    Add header

--- pages/404.php
+++ pages/404.php
@@ -5,18 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Halaman Tidak Ditemukan | GNews</title>
     <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
-    <main class="error-page">
-        <div class="error-content">
-            <h1>404</h1>
-            <h2>Halaman Tidak Ditemukan</h2>
-            <p>
-                Maaf, halaman yang kamu cari tidak ditemukan
-                atau mungkin sudah dipindahkan.
-            </p>
-            <a href="/">Kembali ke Home</a>
-        </div>
-    </main>
+  <?php require_once __DIR__ . "/../components/header.php";?>
+  <main class="error-page">
+    <section class="page-404">
+      <div>
+        <h1>404 :)</h1>
+      </div>
+      <h2>Halaman Tidak Ditemukan</h2>
+      <p>Maaf, halaman yang kamu cari tidak ditemukan atau mungkin sudah dipindahkan.
+      </p>
+      <a href="/">Kembali ke Home</a>
+    </div>
+  </main>
 </body>
 </html>
