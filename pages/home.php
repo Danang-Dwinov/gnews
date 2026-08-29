144153bfcd3d442d8ddb7720548d9403117fc383
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-29 08:31:04 +0700

    Remove login redirect

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/session.php';
+session_start();
 require_once __DIR__ . '/../controllers/newscontrollers.php';
 
 function render_news_cards(array $newsList): void {
@@ -29,7 +29,7 @@ function render_news_cards(array $newsList): void {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>GNews.com</title>
+  <title>GNews</title>
   <link rel="stylesheet" href="../assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
