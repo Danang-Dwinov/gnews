610802e1b1ff0265b4689ccb9afde7cc7d2ad457
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-30 15:03:41 +0700

    Create a news helper to display news on the home page

---
+++ helper/homeRenderNews.php
@@ -0,0 +1,21 @@
+<?php
+function render_news_cards(array $newsList): void {
+  if (empty($newsList)) {
+    echo '<p class="news-card-empty">Belum ada berita untuk kategori ini.</p>';
+    return;
+  }
+  foreach ($newsList as $news) {
+    ?>
+    <article class="news-card">
+      <img src="<?=htmlspecialchars($news['image_url'])?>" class="news-card-image">
+      <div class="news-card-body">
+        <a href="content.php?id=<?=urlencode($news['id'])?>" class="news-card-link">
+          <p class="news-card-text"><?=htmlspecialchars($news['title'])?></p>
+          <p class="news-card-date"><?=htmlspecialchars($news['published_at'])?></p>
+        </a>
+      </div>
+    </article>
+    <?php
+  }
+}
+?>
