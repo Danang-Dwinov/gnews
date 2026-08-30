8d66ed8bcc0f6f98d0c1672daf87968bcc1f6935
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-30 14:52:01 +0700

    Add DOCTYPE and link css

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,20 @@
-<footer class="footer">
-  <div class="footer-list" id="footer-list">
-    <a href="/"><i class="fa-solid fa-house" id="footer-home"></i></a>
-    <a href="/search"><i class="fa-solid fa-magnifying-glass" id="footer-search"></i></a>
-    <a href="/profile"><i class="fa-solid fa-user" id="footer-profile"></i></a>
-  </div>
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+  <footer class="footer">
+    <div class="footer-list" id="footer-list">
+      <a href="/"><i class="fa-solid fa-house" id="footer-home"></i></a>
+      <a href="/search"><i class="fa-solid fa-magnifying-glass" id="footer-search"></i></a>
+      <a href="/profile"><i class="fa-solid fa-user" id="footer-profile"></i></a>
+    </div>
+  </footer>
+</body>
+</html>
