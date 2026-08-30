213a375d2e3ae7d722953f9dd76be50ed70c34ff
Danang-Dwinov <danangdwinov65@gmail.com>
2026-08-30 14:52:14 +0700

    Add DOCTYPE and link css

--- components/back.php
+++ components/back.php
@@ -1,4 +1,17 @@
-<header id="head">
-  <a href="/profile" id="link-back"><i class="fa-solid fa-chevron-left"></i></a>
-  <p id="text"></p>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+  <header id="head">
+    <a href="/profile" id="link-back"><i class="fa-solid fa-chevron-left"></i></a>
+    <p id="text"></p>
+  </header>
+</body>
+</html>
