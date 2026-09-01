ed21dad8bda006b698736fe55e6accf35d43ac38
Danang-Dwinov <danangdwinov65@gmail.com>
2026-09-02 00:19:51 +0700

    Improve input and display content when users search

--- pages/search.php
+++ pages/search.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../config/session.php';
+session_start();
+require_once __DIR__ . "/../controllers/searchcontrollers.php";
+require_once __DIR__ . '/../helper/searchRenderNews.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,13 +18,22 @@ require_once __DIR__ . '/../config/session.php';
     <section>
       <div class="div-search">
         <i class="fa-solid fa-magnifying-glass" id="search-icon"></i>
-        <input type="text" placeholder="Search"id="search-input" name="search">
+        <form action="/search" method="GET">
+          <input type="type" placeholder="Search"id="search-input" name="q" value="<?=htmlspecialchars($keyword)?>">
+        </form>
         <button type="button" id="xmark-button" onclick="clearSearch()">
           <i class="fa-solid fa-times" ></i>
         </button>
       </div>
-      <div class="content-search">
-        
+      <div class="news-list">
+        <?php if($keyword === ''): ?>
+          <p class="news-card-empty">input keyword for search news</p>
+        <?php elseif(empty($searchResults)): ?>
+          <p class="news-card-empty">There is no news that matches "<?=htmlspecialchars($keyword)?>"</p>
+        <?php else: ?>
+          <p class="search-result-count"><?=count($searchResults)?> result for "<?=htmlspecialchars($keyword)?>"</p>
+          <?php render_news_cards($searchResults); ?>
+        <?php endif; ?>
       </div>
     </section>
   </main>
