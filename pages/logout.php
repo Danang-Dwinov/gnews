81d4e8f8cdbebca766a3ca25bb808abb356f8ba8
Danang-Dwinov <danangdwinov65@gmail.com>
2026-09-13 23:51:40 +0700

    Add request server, session user, ans add logout message

--- pages/logout.php
+++ pages/logout.php
@@ -1,11 +1,20 @@
 <?php
-error_log("Masuk Logout");
-
 session_start();
+
+if($_SERVER["REQUEST_METHOD"] !== "POST"){
+  header("Location: /settings");
+  exit;
+}
+if(!isset($_SESSION["username"])){
+  header("Location: /");
+  exit;
+}
 
 session_unset();
 session_destroy();
 
+session_start();
+$_SESSION['pesan'] = "Logout succeed";
 header('Location: /auth');
 exit;
 ?>
