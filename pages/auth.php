0841ce46884bcd3e5322036d982c88b644791041
Danang-Dwinov <danangdwinov65@gmail.com>
2026-09-15 23:13:26 +0700

    Add filter_var for validate format email, and add login messages from each input

--- pages/auth.php
+++ pages/auth.php
@@ -9,12 +9,13 @@ if(isset($_POST['submit-signup'])){
   $email = trim($_POST['email']);
   $password = trim($_POST['password']);
   
+  $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
   $hash_password = password_hash($password, PASSWORD_DEFAULT);
   
   $sql = "INSERT INTO users(full_name, username, email, password) values(?,?,?,?)";
   $stmt = $conn->prepare($sql);
   
-  if($stmt->execute([$full_name, $username, $email, $hash_password])){
+  if($stmt->execute([$full_name, $username, $email_validate, $hash_password])){
     header("Location: /");
     exit();
   } else{
@@ -23,11 +24,16 @@ if(isset($_POST['submit-signup'])){
   }
 }
 
-$login_message = "";
 if(isset($_POST['submit-login'])){
   $username = trim($_POST['username']);
   $email = trim($_POST['email']);
   $password = trim($_POST['password']);
+  
+  $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+  
+  if(!filter_var($email_validate, FILTER_VALIDATE_EMAIL)){
+    $_SESSION['login-email-format-message'] = "Invalid email format";
+  }
   
   $sql = "SELECT * FROM users WHERE username = ?";
   $stmt = $conn->prepare($sql);
@@ -36,23 +42,23 @@ if(isset($_POST['submit-login'])){
   if($stmt->rowCount()>0){
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    if($email==$row['email']){
+    if($email_validate==$row['email']){
       
       if(password_verify($password, $row["password"])){
         $_SESSION['username'] = $username;
         
-        $_SESSION['login-message'] = "Login succeed";
+        $_SESSION['login-succeed-message'] = "Login succeed";
         header("Location: /");
         exit();
         
       } else {
-        $login_message = "Password salah";
+        $_SESSION['login-password-message'] = "Password invalid";
       }
     } else {
-      $login_message = "Email salah";
+      $_SESSION['login-email-message'] = "Email invalid";
     }
   } else {
-    $login_message = "Username salah";
+    $_SESSION['login-username-message'] = "Username invalid";
   }
 }
 ?>
@@ -63,7 +69,7 @@ if(isset($_POST['submit-login'])){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Login</title>
   <link rel="stylesheet" href="../assets/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
@@ -86,15 +92,43 @@ if(isset($_POST['submit-login'])){
           <br>
           <input type="text" id="login-username-input" name="username" placeholder="darren" required title="Input username">
           <br>
+          <?php if(isset($_SESSION['login-username-message'])):?>
+            <div class="message-auth">
+              <i id="text-message-auth"><?=htmlspecialchars($_SESSION['login-username-message'])?></i>
+              <i id="icon-message-auth" class="fa-solid fa-circle-xmark"></i>
+            </div>
+            <?php unset($_SESSION['login-username-message']);?>
+          <?php endif;?>
           <br>
           <label id="login-email-label" for="email">email :</label>
           <br>
           <input type="email" id="login-email-input" name="email" placeholder="darrenjunior44@gmail.com" required title="Input email">
           <br>
+          <?php if(isset($_SESSION['login-email-message'])):?>
+            <div class="message-auth">
+              <i id="text-message-auth"><?=htmlspecialchars($_SESSION['login-email-message'])?></i>
+              <i id="icon-message-auth" class="fa-solid fa-circle-xmark"></i>
+            </div>
+            <?php unset($_SESSION['login-email-message']);?>
+          <?php endif;?>
+          <?php if(isset($_SESSION['login-email-format-message'])):?>
+            <div class="message-auth">
+              <i id="text-message-auth"><?=htmlspecialchars($_SESSION['login-email-format-message'])?></i>
+              <i id="icon-message-auth" class="fa-solid fa-circle-xmark"></i>
+            </div>
+            <?php unset($_SESSION['login-email-format-message']);?>
+          <?php endif;?>
           <br>
           <label id="login-password-label" for="password">password :</label>
           <br>
           <input type="password" id="login-password-input" name="password" placeholder="example : daRReN123#$_" minlegth="8" title="input password">
+          <?php if(isset($_SESSION['login-password-message'])):?>
+            <div class="message-auth">
+              <i id="text-message-auth"><?=htmlspecialchars($_SESSION['login-password-message'])?></i>
+              <i id="icon-message-auth" class="fa-solid fa-circle-xmark"></i>
+            </div>
+            <?php unset($_SESSION['login-password-message']);?>
+          <?php endif;?>
           <div class="password-checkbox">
             <input type="checkbox" id="password-show-label"onclick="showPassword()">
             <label for="password-show-checkbox" id="password-show-label" >Show password</label>
@@ -104,7 +138,6 @@ if(isset($_POST['submit-login'])){
             <label for="login-remember-checkbox" id="login-remember-label">Remember me</label>
             <a href="https://myaccount.google.com/?hl=id" id="login-forgot-password-link">forgot password?</a>
           </div>
-          <p id="form-message"><?= $login_message ?></p>
           <input type="submit" value="Login" id="login-submit-btn" name="submit-login">
           <a href="/" class="back-home"> <i class="fa-solid fa-arrow-left"></i> Back to Home</a>
         </form>
@@ -146,7 +179,6 @@ if(isset($_POST['submit-login'])){
             <input type="checkbox" id="signup-agree-checkbox">
             <label for="signup-agree-checkbox" id="signup-agree-label">I agree to the Terms & Conditions and Privacy Policy</label>
           </div>
-          <i><?= $signup_message ?></i>
           <input type="submit" value="SignUp" id="signup-submit-btn" name="submit-signup">
           <a href="/" class="back-home"> <i class="fa-solid fa-arrow-left"></i> Back to Home</a>
         </form>
