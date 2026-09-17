e5a2b5cf0463fe2b63fa09fdbc32ea9db771109d
Danang-Dwinov <danangdwinov65@gmail.com>
2026-09-17 23:07:06 +0700

    feat: add components include notif to header

--- pages/home.php
+++ pages/home.php
@@ -40,15 +40,7 @@ if(!isset($_SESSION["username"])){
 </head>
 <body>
   <header class="header">
-    <?php if(isset($_SESSION['login-message'])):?>
-      <div class="notif">
-        <p id="login-message-home">
-            <?=htmlspecialchars($_SESSION['login-message'] )?>
-          </p>
-        <i id="icon-message-home" class="fa-solid fa-circle-check"></i>
-      </div>
-      <?php unset($_SESSION["login-message"]);?>
-    <?php endif; ?>
+    <?php include __DIR__ . "/../components/notifications.php";?>
     <h2 id="header-title">GNews.com</h2>
   </header>
   <main>
