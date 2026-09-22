75b711be0371368c0ecaebe0e1cc52f58b46fdc7
Danang-Dwinov <danangdwinov65@gmail.com>
2026-09-22 23:29:05 +0700

    refactor: move and searate logic login

--- pages/auth.php
+++ pages/auth.php
@@ -1,67 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
-session_start();
-
-if(isset($_POST['submit-signup'])){
-  $full_name = $_POST['name'];
-  $username = trim($_POST['username']);
-  $email = trim($_POST['email']);
-  $password = trim($_POST['password']);
-  
-  $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
-  $hash_password = password_hash($password, PASSWORD_DEFAULT);
-  
-  $sql = "INSERT INTO users(full_name, username, email, password) values(?,?,?,?)";
-  $stmt = $conn->prepare($sql);
-  
-  if($stmt->execute([$full_name, $username, $email_validate, $hash_password])){
-    $_SESSION['login-notif-message'] = 'Signup successful, please login';
-    header("Location: /auth");
-    exit;
-  } else{
-    $_SESSION['login-notif-message'] = 'Signup fail';
-    header("Location: /auth");
-    exit;
-  }
-}
-
-if(isset($_POST['submit-login'])){
-  $username = trim($_POST['username']);
-  $email = trim($_POST['email']);
-  $password = trim($_POST['password']);
-  
-  $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
-  
-  if(!filter_var($email_validate, FILTER_VALIDATE_EMAIL)){
-    $_SESSION['login-email-format-message'] = "Invalid email format";
-  }
-  
-  $sql = "SELECT * FROM users WHERE username = ?";
-  $stmt = $conn->prepare($sql);
-  $stmt->execute([$username]);
-  
-  if($stmt->rowCount()>0){
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-    if($email_validate==$row['email']){
-      
-      if(password_verify($password, $row["password"])){
-        $_SESSION['username'] = $username;
-        
-        $_SESSION['login-notif-message'] = "Login succeed";
-        header("Location: /");
-        exit();
-        
-      } else {
-        $_SESSION['login-password-message'] = "Password invalid";
-      }
-    } else {
-      $_SESSION['login-email-message'] = "Email invalid";
-    }
-  } else {
-    $_SESSION['login-username-message'] = "Username invalid";
-  }
-}
+require_once __DIR__ . '/../controllers/AuthControllers.php';
 ?>
 
 <!DOCTYPE html>
@@ -75,10 +13,7 @@ if(isset($_POST['submit-login'])){
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
-  <header class="header">
-    <?php include __DIR__ . "/../components/notifications.php";?>
-    <h2 id="header-title">GNews.com</h2>
-  </header>
+  <?php include __DIR__ . '/../components/header.php';?>
   <main>
     <section class="login-page" id="login-page">
       <div class="container-main" align="center">
